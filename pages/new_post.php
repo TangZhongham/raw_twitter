e971e30208d077cd997d334888ba03172ea20a64
tzh <tangzhongham@gmail.com>
2024-03-27 15:27:50 -0400

    use real session id for new_post

--- pages/new_post.php
+++ pages/new_post.php
@@ -4,7 +4,7 @@
    session_start();
 
    // Check if the user is not logged in
-   if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+   if (!isset($_SESSION['user_id'])) {
        // Display a message and redirect to the login page
        echo '<script>alert("Please log in to view this page");';
        echo 'window.location.href = ".";</script>';
